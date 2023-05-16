76ba4d1a143f32ab2d8f39cddaa217271ed3df83
Umar Ashraf <92431008+Umar-Ashraf09@users.noreply.github.com>
2023-05-16 18:17:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app = express();
-dotenv.config();
+dotenv.config({ path: "./.env"});
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
